f54cfd4b3f72b2e25ff4c68905a40894e5e600f8
Markéta Musílková <mrs.musilkova@gmail.com>
2024-04-12 18:40:09 +0200

    Úprava ikon v horním menu, přidání křížku

--- spolecne.js
+++ spolecne.js
@@ -1,5 +1,17 @@
 
-const hamburger = document.getElementById("menu-tlacitko").addEventListener("click", () => {
-  document.getElementById("menu-polozky").classList.toggle("show")
-})
+// const hamburger = document.getElementById("menu-tlacitko").addEventListener("click", () => {
+//   document.getElementById("menu-polozky").classList.toggle("show")
+// })
 
+// BONUS (výměna ikony hamburgeru a křížku)
+const menuTlacitkoElm = document.querySelector('#menu-tlacitko')
+const menuPolozkyElm = document.querySelector('#menu-polozky')
+menuTlacitkoElm.addEventListener('click', () => {
+	menuPolozkyElm.classList.toggle('show')
+
+	if (menuPolozkyElm.classList.contains('show')) {
+		menuTlacitkoElm.innerHTML = '<i class="fas fa-xmark"></i>'
+	} else {
+		menuTlacitkoElm.innerHTML = '<i class="fas fa-bars"></i>'
+	}
+})
